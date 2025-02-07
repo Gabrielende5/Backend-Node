4d6b7b7129c86e6d0af2c0321fdab2f11d05581b
Gabrielende5 <gabrielende53@gmail.com>
2025-02-07 11:03:09 -0300

    atualização

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ import productrouter from "./routes/ProductRouter.js"
 import router from "./routes/UserRouter.js";
 import comidaRouter from "./routes/ComidaRouter.js";
 import pokemonRouter from "./routes/PokemonRouter.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+
 config();
 
 // Procurando arquivos
@@ -32,6 +36,40 @@ const port = process.env.PORT || 4444;
 // Middleware para servir arquivos estáticos
 app.use(express.static(join(__dirname, "public"))); // Diz que o "/" é a pagina inicial + falando que é public, ou seja, o server.js pode acessar tudo dentro desta pasta
 
+const server = createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:'http://localhost:3000',
+        methods:["GET", "POST"],
+    }
+});
+
+io.on("connection",(socket) =>{
+    console.log(`Usuário conectado! ID ${socket.id}`);
+
+    socket.on("disconnect",() =>{
+        console.log("usuário desconectado ID",socket.id);
+    })
+
+    socket.on("set_username",(username)=>{
+        socket.data.username=username;
+
+    })
+
+    socket.on("message",(text)=>{
+       io.emit("receive_message", {
+            ...text,
+            authorID:socket.id,
+            author:socket.data.username
+        })
+    })
+});
+
+server.listen(8080, ()=>{
+    console.log("Socket.io está rodando na porta 8080");
+})
+
 // Coloca a rota em uso
 app.use("/exemplo", testRouter);
 app.use("/products", productrouter);
@@ -46,3 +84,4 @@ app.listen(port, () => {
 });
 
 connectDatabase();
+
